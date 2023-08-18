96f38d510d8ac3af323c31e5400418fd6f183f2f
Cyrille Pierre <cyrille.pierre@inrae.fr>
2023-08-18 14:08:18 +0200

    Create romea_path_matching_bringup package

---
+++ romea_path_matching_bringup/launch/path_matching.launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import LifecycleNode
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument('path', description="filename of the path to follow"),
+        LifecycleNode(
+            package='romea_path_matching',
+            executable='path_matching_node',
+            name='path_matching',
+            exec_name='path_matching',
+            namespace='alpo',
+            parameters=[{
+                'autostart': True,
+                'path': LaunchConfiguration('path'),
+            }],
+            remappings=[
+                ('odom', 'localisation/filtered_odom'),
+            ],
+        )
+    ])
